b1b6341e3f15214bc3928f773f783732f8a5c0a9
Anna Valerio <anna.j.neff@gmail.com>
2024-12-05 19:42:19 -0600

    First Commit

---
+++ src/main/java/org/launchcode/demo/LittleOnlineLibraryApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleOnlineLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleOnlineLibraryApplication.class, args);
+	}
+
+}
