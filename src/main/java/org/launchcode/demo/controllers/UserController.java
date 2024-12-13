542027dbbc6d2ecac785c92a62894e64e2bc39eb
Carolyn Smith <carolynsmith@slu.edu>
2024-12-12 18:42:50 -0600

    User Profile MVC rough start

---
+++ src/main/java/org/launchcode/demo/controllers/UserController.java
@@ -0,0 +1,37 @@
+package org.launchcode.controllers;
+
+import jakarta.validation.Valid;
+import org.launchcode.demo.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
+        return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @Valid @ModelAttribute User user, Errors errors, String verify) {
+        model.addAttribute(new User());
+        if(errors.hasErrors()) {
+            return "user";
+        }
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+        }
+        else {
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+
+    }
+
+
+}
---
+++ src/main/java/org/launchcode/demo/models/User.java
@@ -0,0 +1,75 @@
+package org.example.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotBlank
+    @Size(min = 5, max = 15)
+    private String username;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    @NotBlank
+    @Size(min = 6)
+    private String verify;
+
+    public User() {
+
+    }
+
+    public User(String username, String email, String password, String verify) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.verify = verify;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVerify() { return verify; }
+
+    public void setVerify(String verify) { this.verify = verify; }
+}
+
+
