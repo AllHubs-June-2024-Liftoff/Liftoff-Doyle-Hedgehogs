c47e0bc90ea1c7196592dd917423e51b50fc1b3b
MET <matthew.etracy@gmail.com>
2024-12-23 19:02:30 -0600

    Okay I have the data inserted into the table now and am now actually going to try to make a query that will pull all the information from each table that will display the location, unique_book, username, email address, etc.

--- booksAPI2.sql
+++ booksAPI2.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS BooksAPI;
-
+DROP DATABASE IF EXISTS BOOKSAPI;
 CREATE DATABASE BOOKSAPI;
 
 USE BOOKSAPI;
@@ -7,7 +7,7 @@ USE BOOKSAPI;
 CREATE TABLE UserAccounts (
                               id INT AUTO_INCREMENT PRIMARY KEY,
                               username VARCHAR(100) UNIQUE NOT NULL,
-                              location VARCHAR(2), -- 0: Kansas City 1: Philadelphia 2: St. Louis
+                              location VARCHAR(2), -- 00: Kansas City 01: Philadelphia 02: St. Louis
                               email VARCHAR(100) UNIQUE NOT NULL,
                               password_hash VARCHAR(255) NOT NULL
 );
@@ -22,15 +22,16 @@ CREATE TABLE Volumes (
 
 CREATE TABLE Bookshelves (  -- Each bookshelf is associated with a specific user, can each user can have more than one bookshelf
                              id INT AUTO_INCREMENT PRIMARY KEY,
-                             bookshelf_id VARCHAR(255) NOT NULL, -- what they name their specific bookshelf, "i.e. AJ's bookshelf favorites"
+                             bookshelf_name VARCHAR(255) NOT NULL, -- what they name their specific bookshelf, "i.e. AJ's bookshelf favorites"
                              user_id INT,
                              FOREIGN KEY (user_id) REFERENCES UserAccounts(id)
 );
 
 CREATE TABLE BookshelfVolumes (
                                   bookshelf_id INT, -- the bookshelf ID that belongs to a specific user. This way each user could have more than one bookshelf/
-                                  volume_id VARCHAR(50), -- this will be the unique exact book.
+                                  volume_id VARCHAR(50), -- this will be the abstracted book.
                                   has_book BOOLEAN, -- TRUE means that person has the book currently, FALSE means person does not have the book on their current bookshelf.
+                                  unique_book VARCHAR(255) AS (CONCAT(bookshelf_id, '_', volume_id)),
                                   -- book_location VARCHAR(2),
                                   -- volume_author VARCHAR(50),
                                   -- volume_title VARCHAR(255) NOT NULL,
@@ -48,4 +49,38 @@ CREATE TABLE BookshelfVolumes (
 );
 -- 12/20/2024 Let me know what everyone thinks of this, this is able to work in mysql workbench
 
---
+
+/* Adding three books (two are kind of the same book)
+userAccounts data, and a bookshelfVolume data */
+INSERT INTO Volumes (id, author, title, description, thumbnail) VALUES
+('3fOWbIrdRdIC', 'Jane Austen, Seth Grahame-Smith', 'Pride and Prejudice and Zombies: The Deluxe Heirloom Edition',
+ 'The deluxe heirloom edition of the "New York Times" bestseller boasts additional scenes of zombie mayhem, 13 new full-color illustrations, and an essay Afterword by Dr. Allen Grove, Professor of English Literature.',
+ 'http://books.google.com/books/content?id=3fOWbIrdRdIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
+
+('g6C6P6NJQ1MC', 'Steve Hockensmith', 'Pride and Prejudice and Zombies: Dreadfully Ever After',
+ 'Complete with romance, heartbreak, martial arts, cannibalism, and an army of shambling corpses, Dreadfully Ever After brings the story of Pride and Prejudice and Zombies to a thrilling conclusion.',
+ 'http://books.google.com/books/content?id=g6C6P6NJQ1MC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
+
+('KUMIEAAAQBAJ', 'Khaled Hosseini', 'The Kite Runner',
+'THE NUMBER ONE BESTSELLER \'Devastating\' Daily Telegraph \'Heartbreaking\' The Times \'Unforgettable\' Isabel Allende \'Haunting\' Independent Afghanistan, 1975: Twelve-year-old Amir is desperate to win the local kite-fighting tournament and his loyal friend Hassan promises to help him. But neither of the boys can foresee what will happen to Hassan that afternoon, an event that is to shatter their lives. After the Russians invade and the family is forced to flee to America, Amir realises that one day he must return to Afghanistan under Taliban rule to find the one thing that his new world cannot grant him: redemption.',
+'http://books.google.com/books/content?id=KUMIEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api');
+
+/* Adding userdata for myself and a made-up password hash
+location is kansas_city 01
+*/
+INSERT INTO UserAccounts (username, location, email, password_hash) VALUES 
+('mattet', '00', 'mattetracy@outlook.com', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S'); -- I don't need to put the ID since it is autoincremented
+
+
+INSERT INTO Bookshelves (bookshelf_name, user_id) VALUES
+("Matt's Good Reads", 1); -- I don't need to put the ID since it is autoincremented
+
+INSERT INTO BookshelfVolumes (bookshelf_id, volume_id, has_book) VALUES
+(1, 'KUMIEAAAQBAJ', TRUE); -- I don't need to put the ID since it is autoincremented
+
+
+
+
+
+SELECT * FROM Bookshelves;
+SELECT * FROM BookshelfVolumes
