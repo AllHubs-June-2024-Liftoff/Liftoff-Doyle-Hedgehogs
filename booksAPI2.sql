061ad2119e87d0e8aa93c6d65e3f746c6fc34f00
MET <matthew.etracy@gmail.com>
2024-12-24 13:58:54 -0600

    SQL query was buggered up. Was incorrectly attaching every book on any shelf with every user.  Added the conditoinal statements on the join clauses to only have the UserAccounts.id = Bookshelves.bookshelves_user_id and then carried down to the next join clause where Bookshelves.id = BookshelfVolumes.bookshelf_id

--- booksAPI2.sql
+++ booksAPI2.sql
@@ -23,8 +23,8 @@ CREATE TABLE Volumes (
 CREATE TABLE Bookshelves (  -- Each bookshelf is associated with a specific user, can each user can have more than one bookshelf
                              id INT AUTO_INCREMENT PRIMARY KEY,
                              bookshelf_name VARCHAR(255) NOT NULL, -- what they name their specific bookshelf, "i.e. AJ's bookshelf favorites"
-                             user_id INT,
-                             FOREIGN KEY (user_id) REFERENCES UserAccounts(id)
+                             bookshelves_user_id INT,
+                             FOREIGN KEY (bookshelves_user_id) REFERENCES UserAccounts(id)
 );
 
 CREATE TABLE BookshelfVolumes (
@@ -62,28 +62,69 @@ INSERT INTO Volumes (id, author, title, description, thumbnail) VALUES
  'http://books.google.com/books/content?id=g6C6P6NJQ1MC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
 
 ('KUMIEAAAQBAJ', 'Khaled Hosseini', 'The Kite Runner',
-'THE NUMBER ONE BESTSELLER \'Devastating\' Daily Telegraph \'Heartbreaking\' The Times \'Unforgettable\' Isabel Allende \'Haunting\' Independent Afghanistan, 1975: Twelve-year-old Amir is desperate to win the local kite-fighting tournament and his loyal friend Hassan promises to help him. But neither of the boys can foresee what will happen to Hassan that afternoon, an event that is to shatter their lives. After the Russians invade and the family is forced to flee to America, Amir realises that one day he must return to Afghanistan under Taliban rule to find the one thing that his new world cannot grant him: redemption.',
-'http://books.google.com/books/content?id=KUMIEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api');
+"THE NUMBER ONE BESTSELLER 'Devastating' Daily Telegraph 'Heartbreaking' The Times 'Unforgettable' Isabel Allende 'Haunting' Independent Afghanistan, 1975: Twelve-year-old Amir is desperate to win the local kite-fighting tournament and his loyal friend Hassan promises to help him. But neither of the boys can foresee what will happen to Hassan that afternoon, an event that is to shatter their lives. After the Russians invade and the family is forced to flee to America, Amir realises that one day he must return to Afghanistan under Taliban rule to find the one thing that his new world cannot grant him: redemption.",
+'http://books.google.com/books/content?id=KUMIEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
 
-/* Adding userdata for myself and a made-up password hash
+('kotPYEqx7kMC', 'George Orwell', '1984',
+'75th ANNIVERSARY EDITION “Orwell saw, to his credit, that the act of falsifying reality is only secondarily a way of changing perceptions. It is, above all, a way of asserting power.” In 1984, London is a grim city in the totalitarian state of Oceania where Big Brother is always watching you and the Thought Police can practically read your mind. Winston Smith is a man in grave danger for the simple reason that his memory still functions. Drawn into a forbidden love affair, Winston finds the courage to join a secret revolutionary organization called The Brotherhood, dedicated to the destruction of the Party. Together with his beloved Julia, he hazards his life in a deadly match against the powers that be.',
+    'http://books.google.com/books/content?id=kotPYEqx7kMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
+
+('wrOQLV6xB-wC', 'J.K. Rowling', "Harry Potter and the Sorcerer's Stone",
+"Turning the envelope over, his hand trembling, Harry saw a purple wax seal bearing a coat of arms; a lion, an eagle, a badger and a snake surrounding a large letter 'H'. Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle. Then, on Harry's eleventh birthday, a great beetle-eyed giant of a man called Rubeus Hagrid bursts in with some astonishing news: Harry Potter is a wizard, and he has a place at Hogwarts School of Witchcraft and Wizardry. An incredible adventure is about to begin! Having become classics of our time, the Harry Potter eBooks never fail to bring comfort and escapism. With their message of hope, belonging and the enduring power of truth and love, the story of the Boy Who Lived continues to delight generations of new readers.",
+'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
+
+('PGR2AwAAQBAJ', 'Harper Lee', 'To Kill a Mockingbird',
+    "Harper Lee's Pulitzer Prize-winning masterwork of honor and injustice in the deep South—and the heroism of one man in the face of blind and violent hatred. One of the most cherished stories of all time, To Kill a Mockingbird has been translated into more than forty languages, sold more than forty million copies worldwide, served as the basis for an enormously popular motion picture, and was voted one of the best novels of the twentieth century by librarians across the country. A gripping, heart-wrenching, and wholly remarkable tale of coming-of-age in a South poisoned by virulent prejudice, it views a world of great beauty and savage inequities through the eyes of a young girl, as her father—a crusading local lawyer—risks everything to defend a black man unjustly accused of a terrible crime.",
+    'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+);
+/* Adding userdata for myself and a made-up password hash from BCrypt
 location is kansas_city 00
 */
-INSERT INTO UserAccounts (username, location, email, password_hash) VALUES 
+INSERT INTO UserAccounts (username, location, email, password_hash) VALUES
 ('mattet', '00', 'mattetracy@outlook.com', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S'); -- I don't need to put the ID since it is autoincremented
-
-
-INSERT INTO Bookshelves (bookshelf_name, user_id) VALUES
+INSERT INTO Bookshelves (bookshelf_name, bookshelves_user_id) VALUES
 ("Matt's Good Reads", 1); -- I don't need to put the ID since it is autoincremented
-
 INSERT INTO BookshelfVolumes (bookshelf_id, volume_id, has_book) VALUES
 (1, 'KUMIEAAAQBAJ', TRUE);
 
+/* Adding userdata for random person and a made-up password hash from BCrypt
+location is St. Louis 02
+*/
+INSERT INTO UserAccounts (username, location, email, password_hash) VALUES
+('bob2342', '02', 'bob2342@gmail.com', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S'); -- I don't need to put the ID since it is autoincremented
+INSERT INTO Bookshelves (bookshelf_name, bookshelves_user_id) VALUES
+("Bob's Good Reads", 1); -- I don't need to put the ID since it is autoincremented
+INSERT INTO BookshelfVolumes (bookshelf_id, volume_id, has_book) VALUES
+(2, 'g6C6P6NJQ1MC', TRUE);
 
+/* Adding userdata for random person and a made-up password hash from BCrypt
+location is St. Louis 02
+*/
+INSERT INTO UserAccounts (username, location, email, password_hash) VALUES
+('alice4242', '02', 'alice4242@gmail.com', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S'); -- I don't need to put the ID since it is autoincremented
+INSERT INTO Bookshelves (bookshelf_name, bookshelves_user_id) VALUES
+("Alice's Good Reads", 3); -- I don't need to put the ID since it is autoincremented
+INSERT INTO BookshelfVolumes (bookshelf_id, volume_id, has_book) VALUES
+(3, 'PGR2AwAAQBAJ', TRUE);
 
+INSERT INTO UserAccounts (username, location, email, password_hash) VALUES
+('doyle', '02', 'doyle@launchcode.org', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S'); -- I don't need to put the ID since it is autoincremented
+INSERT INTO Bookshelves (bookshelf_name, bookshelves_user_id) VALUES
+("Doyle's Good Reads", 4); -- I don't need to put the ID since it is autoincremented
+INSERT INTO BookshelfVolumes (bookshelf_id, volume_id, has_book) VALUES
+(4, 'KUMIEAAAQBAJ', TRUE);
+
+INSERT INTO UserAccounts (username, location, email, password_hash) VALUES
+('emily', '02', 'emilyu@launchcode.org', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S'); -- I don't need to put the ID since it is autoincremented
+INSERT INTO Bookshelves (bookshelf_name, bookshelves_user_id) VALUES
+("Emily's Good Reads", 5); -- I don't need to put the ID since it is autoincremented
+INSERT INTO BookshelfVolumes (bookshelf_id, volume_id, has_book) VALUES
+(5, 'wrOQLV6xB-wC', TRUE);
 
 -- this query will give us the information for all user_id, and all the book data we need across all of every users bookshelves (all their bookshelfs)
-SELECT 
+SELECT
     UserAccounts.id AS user_id,
+    Bookshelves.bookshelves_user_id,
     UserAccounts.username AS username,
     UserAccounts.email AS email,
     UserAccounts.location AS location,
@@ -91,17 +132,20 @@ SELECT
     Bookshelves.bookshelf_name AS bookshelf_name,
     BookshelfVolumes.volume_id AS volume_id,
     BookshelfVolumes.has_book AS has_book,
+    BookshelfVolumes.unique_book,
     Volumes.author AS author,
     Volumes.title AS title,
     Volumes.description AS description,
     Volumes.thumbnail AS thumbnail
-FROM 
+FROM
     UserAccounts
-JOIN 
-    Bookshelves ON user_id = Bookshelves.user_id
-JOIN 
-    BookshelfVolumes ON bookshelf_id = BookshelfVolumes.bookshelf_id
-JOIN 
-    Volumes ON volume_id = Volumes.id
-ORDER BY 
-    location, username, title;
+JOIN
+    Bookshelves ON UserAccounts.id = Bookshelves.bookshelves_user_id -- fixed output result of SQL query, needed conditional
+JOIN
+    BookshelfVolumes ON Bookshelves.id = BookshelfVolumes.bookshelf_id -- fixed output result of SQL query, needed conditional
+JOIN
+    Volumes ON BookshelfVolumes.volume_id = Volumes.id
+WHERE
+    BookshelfVolumes.has_book = TRUE
+ORDER BY
+    UserAccounts.location, UserAccounts.username, Volumes.title;
