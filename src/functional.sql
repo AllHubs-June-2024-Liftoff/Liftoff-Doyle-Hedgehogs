c7b797a0390f565494593f3523aaea45d8f33c10
MET <mattetracy@outlook.com>
2025-01-27 19:34:30 -0600

    Create functional.sql
    -- This is compatible with version 2a3453edfec5e49666483c0895a6732a274ab188 version of devchanges. This has functional triggers that work when update, insert, or delete a rating.

---
+++ src/functional.sql
@@ -0,0 +1,272 @@
+-- This is compatible with version 2a3453edfec5e49666483c0895a6732a274ab188 version of devchanges. This has functional triggers that work when update, insert, or delete a rating.
+
+DROP DATABASE IF EXISTS BooksAPI;
+
+DROP DATABASE IF EXISTS BOOKSAPI;
+DROP DATABASE IF EXISTS booksapi;
+CREATE DATABASE booksapi;
+USE booksapi;
+
+CREATE TABLE location (
+						id INT AUTO_INCREMENT PRIMARY KEY,
+                        name VARCHAR(255) UNIQUE NOT NULL
+);
+
+INSERT into location VALUES (1, 'Kansas City');
+INSERT into location VALUES (2, 'Philadelphia');
+INSERT into location VALUES (3, 'St. Louis');
+
+
+
+CREATE TABLE user (
+                              id INT AUTO_INCREMENT PRIMARY KEY,
+                              username VARCHAR(255) UNIQUE NOT NULL,
+                              location_id INT, -- '0': Kansas City '1': Philadelphia '2': St. Louis
+                              email VARCHAR(255) UNIQUE NOT NULL,
+                              pwhash VARCHAR(255) NOT NULL,
+                              FOREIGN KEY (location_id) REFERENCES location(id)
+);
+
+CREATE TABLE volume (
+                        id VARCHAR(255) PRIMARY KEY, -- Google booksAPI's unique string
+                        author VARCHAR(255), -- Author associated with unique book.
+                        title VARCHAR(255) NOT NULL, -- title associated with unique book
+                        description TEXT, -- description from google unique string description of book
+                        rating_avg FLOAT DEFAULT NULL,
+                        thumbnail TEXT -- link to normal res book photo thumbnail
+);
+
+CREATE TABLE bookshelf (  -- Each bookshelf is associated with a specific user, can each user can have more than one bookshelf
+                           id INT AUTO_INCREMENT PRIMARY KEY,
+                           bookshelf_name VARCHAR(255) NOT NULL, -- what they name their specific bookshelf, "i.e. AJ's bookshelf favorites"
+                           user_id INT,
+                           FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE bookshelf_volume (
+                                  id INT AUTO_INCREMENT PRIMARY KEY,
+                                  bookshelf_id INT, -- the bookshelf ID that belongs to a specific user. This way each user could have more than one bookshelf/
+                                  volume_id VARCHAR(255), -- this will be the abstracted book.
+                                  has_book BOOLEAN, -- TRUE means that person has the book currently, FALSE means person does not have the book on their current bookshelf.
+                                  unique_book VARCHAR(255) AS (CONCAT(bookshelf_id, '_', volume_id)),
+                                  ratings FLOAT DEFAULT NULL CHECK (ratings BETWEEN 0.0 AND 5.0),
+                                  FOREIGN KEY (bookshelf_id) REFERENCES bookshelf(id), -- referencing has to be the primary key
+                                  FOREIGN KEY (volume_id) REFERENCES volume(id) -- referencing has to be the primary key
+);
+
+DELIMITER $$
+
+CREATE TRIGGER update_rating_avg_on_delete
+AFTER DELETE ON bookshelf_volume
+FOR EACH ROW
+BEGIN
+    UPDATE volume
+    SET rating_avg = (
+        SELECT ROUND(AVG(bookshelf_volume.ratings), 1)
+        FROM bookshelf_volume
+        WHERE bookshelf_volume.volume_id = OLD.volume_id AND ratings IS NOT NULL
+    )
+    WHERE volume.id = OLD.volume_id;
+END $$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER update_rating_avg_on_update
+AFTER UPDATE ON bookshelf_volume
+FOR EACH ROW
+BEGIN
+    UPDATE volume
+    SET rating_avg = (
+        SELECT ROUND(AVG(bookshelf_volume.ratings), 1)
+        FROM bookshelf_volume
+        WHERE bookshelf_volume.volume_id = NEW.volume_id AND ratings IS NOT NULL
+    )
+    WHERE volume.id = NEW.volume_id;
+END $$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER update_rating_avg_on_insert
+AFTER INSERT ON bookshelf_volume
+FOR EACH ROW
+BEGIN
+    UPDATE volume
+    SET rating_avg = (
+        SELECT ROUND(AVG(bookshelf_volume.ratings), 1)
+        FROM bookshelf_volume
+        WHERE bookshelf_volume.volume_id = NEW.volume_id AND ratings IS NOT NULL
+    )
+    WHERE volume.id = NEW.volume_id;
+END $$
+
+DELIMITER ;
+
+/* Adding three books (two are kind of the same book)
+userAccounts data, and a bookshelfVolume data */
+INSERT INTO volume (id, author, title, description, thumbnail) VALUES
+                                                                   ('3fOWbIrdRdIC', 'Jane Austen, Seth Grahame-Smith', 'Pride and Prejudice and Zombies: The Deluxe Heirloom Edition',
+                                                                    'The deluxe heirloom edition of the "New York Times" bestseller boasts additional scenes of zombie mayhem, 13 new full-color illustrations, and an essay Afterword by Dr. Allen Grove, Professor of English Literature.',
+                                                                    'http://books.google.com/books/content?id=3fOWbIrdRdIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
+
+                                                                   ('g6C6P6NJQ1MC', 'Steve Hockensmith', 'Pride and Prejudice and Zombies: Dreadfully Ever After',
+                                                                    'Complete with romance, heartbreak, martial arts, cannibalism, and an army of shambling corpses, Dreadfully Ever After brings the story of Pride and Prejudice and Zombies to a thrilling conclusion.',
+                                                                    'http://books.google.com/books/content?id=g6C6P6NJQ1MC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
+
+                                                                   ('KUMIEAAAQBAJ', 'Khaled Hosseini', 'The Kite Runner',
+                                                                    'THE NUMBER ONE BESTSELLER',
+                                                                    'http://books.google.com/books/content?id=KUMIEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
+
+                                                                   ('kotPYEqx7kMC', 'George Orwell', '1984',
+                                                                    '75th ANNIVERSARY EDITION',
+                                                                    'http://books.google.com/books/content?id=kotPYEqx7kMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
+
+                                                                   ('wrOQLV6xB-wC', 'J.K. Rowling', "Harry Potter and the Sorcerer's Stone",
+                                                                    "Turning the envelope over...",
+                                                                    'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),
+
+                                                                   ('PGR2AwAAQBAJ', 'Harper Lee', 'To Kill a Mockingbird',
+                                                                    "Harper Lee's Pulitzer Prize-winning masterwork of honor and injustice in the deep South—and the heroism of one man in the face of blind and violent hatred.",
+                                                                    'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+                                                                   );
+                                                                   
+/* Adding userdata for myself and a made-up password hash from BCrypt
+location is Kansas City
+*/
+
+INSERT INTO user (username, location_id, email, pwhash) VALUES
+    ('mattet', 1, 'mattetracy@outlook.com', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S'); 
+INSERT INTO bookshelf (bookshelf_name, user_id) VALUES
+    ("Matt's Good Reads", 1);
+INSERT INTO bookshelf_volume (bookshelf_id, volume_id, has_book, ratings) VALUES
+    (1, '3fOWbIrdRdIC', TRUE, 4.5),
+    (1, 'g6C6P6NJQ1MC', TRUE, 3.8),
+    (1, 'KUMIEAAAQBAJ', TRUE, 4.2),
+    (1, 'kotPYEqx7kMC', TRUE, 4.0),
+    (1, 'PGR2AwAAQBAJ', TRUE, 4.7),
+    (1, 'wrOQLV6xB-wC', TRUE, 3.9);
+
+/* Adding userdata for random person and a made-up password hash from BCrypt
+location is St. Louis
+*/
+
+INSERT INTO user (username, location_id, email, pwhash) VALUES
+    ('bob2342', 3, 'bob2342@gmail.com', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S'); 
+INSERT INTO bookshelf (bookshelf_name, user_id) VALUES
+    ("Bob's Good Reads", 2);
+INSERT INTO bookshelf_volume (bookshelf_id, volume_id, has_book, ratings) VALUES
+    (2, '3fOWbIrdRdIC', TRUE, 4.1),
+    (2, 'g6C6P6NJQ1MC', TRUE, 3.9),
+    (2, 'KUMIEAAAQBAJ', TRUE, 4.3),
+    (2, 'kotPYEqx7kMC', TRUE, 4.5),
+    (2, 'PGR2AwAAQBAJ', TRUE, 4.6),
+    (2, 'wrOQLV6xB-wC', TRUE, 3.7);
+
+/* Adding userdata for random person and a made-up password hash from BCrypt
+location is St. Louis
+*/
+INSERT INTO user (username, location_id, email, pwhash) VALUES
+    ('alice4242', 3, 'alice4242@gmail.com', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S'); 
+INSERT INTO bookshelf (bookshelf_name, user_id) VALUES
+    ("Alice's Good Reads", 3);
+INSERT INTO bookshelf_volume (bookshelf_id, volume_id, has_book, ratings) VALUES
+    (3, '3fOWbIrdRdIC', TRUE, 4.8),
+    (3, 'g6C6P6NJQ1MC', TRUE, 4.0),
+    (3, 'KUMIEAAAQBAJ', TRUE, 4.1),
+    (3, 'kotPYEqx7kMC', TRUE, 3.9),
+    (3, 'PGR2AwAAQBAJ', TRUE, 4.3),
+    (3, 'wrOQLV6xB-wC', TRUE, 4.2);
+
+/* Adding userdata for another random person and a made-up password hash from BCrypt
+location is St. Louis
+*/
+INSERT INTO user (username, location_id, email, pwhash) VALUES
+    ('doyle', 3, 'doyle@launchcode.org', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S');
+INSERT INTO bookshelf (bookshelf_name, user_id) VALUES
+    ("Doyle's Good Reads", 4);
+INSERT INTO bookshelf_volume (bookshelf_id, volume_id, has_book, ratings) VALUES
+    (4, '3fOWbIrdRdIC', TRUE, 3.5),
+    (4, 'g6C6P6NJQ1MC', TRUE, 4.2),
+    (4, 'KUMIEAAAQBAJ', TRUE, 4.0),
+    (4, 'kotPYEqx7kMC', TRUE, 3.8),
+    (4, 'PGR2AwAAQBAJ', TRUE, 4.6),
+    (4, 'wrOQLV6xB-wC', TRUE, 4.4);
+
+/* Adding userdata for another random person and a made-up password hash from BCrypt
+location is St. Louis
+*/
+INSERT INTO user (username, location_id, email, pwhash) VALUES
+    ('emily', 3, 'emilyu@launchcode.org', '$2a$10$7nT.LzAErkRf8nQuvDLP5OGW/R2fRS03zB6F8kMG/lCVdXsJ5lK.S'); 
+INSERT INTO bookshelf (bookshelf_name, user_id) VALUES
+    ("Emily's Good Reads", 5);
+INSERT INTO bookshelf_volume (bookshelf_id, volume_id, has_book, ratings) VALUES
+    (5, '3fOWbIrdRdIC', TRUE, 4.2),
+    (5, 'g6C6P6NJQ1MC', TRUE, 4.7),
+    (5, 'KUMIEAAAQBAJ', TRUE, 4.9),
+    (5, 'kotPYEqx7kMC', TRUE, 4.4),
+    (5, 'PGR2AwAAQBAJ', TRUE, 4.8),
+    (5, 'wrOQLV6xB-wC', TRUE, 3.6);
+
+-- tag some books for functional testing
+-- INSERT INTO bookshelf_volume_tags (bookshelf_volumes_id, tags_id) VALUES
+-- (1, 1),
+-- (1, 3),
+-- (3, 2),
+-- (3, 3),
+-- (4, 3),
+-- (5, 2),
+-- (5, 3);
+
+-- this query will give us the information for all user_id, and all the book data we need across all of every users bookshelves (all their bookshelfs)
+SELECT
+    user.id,
+    bookshelf.user_id,
+    user.username AS username,
+    user.email AS email,
+    user.location_id AS location,
+    
+    bookshelf.id AS bookshelf_id,
+    bookshelf.bookshelf_name AS bookshelf_name,
+    bookshelf_volume.volume_id AS volume_id,
+    bookshelf_volume.has_book AS has_book,
+    bookshelf_volume.unique_book,
+    bookshelf_volume.ratings,
+    volume.author AS author,
+    volume.title AS title,
+    volume.description AS description,
+    volume.thumbnail AS thumbnail
+
+FROM
+    user
+        JOIN
+    bookshelf ON user.id = bookshelf.user_id -- fixed output result of SQL query, needed conditional
+        JOIN
+    bookshelf_volume ON bookshelf.id = bookshelf_volume.bookshelf_id -- fixed output result of SQL query, needed conditional
+        JOIN
+    volume ON bookshelf_volume.volume_id = volume.id
+WHERE
+    bookshelf_volume.has_book = TRUE
+ORDER BY
+    user.location_id, user.username, volume.title;
+
+
+SELECT * FROM VOLUME;
+
+UPDATE bookshelf_volume
+SET ratings = 5
+WHERE bookshelf_id = 5 AND volume_id = '3fOWbIrdRdIC';
+
+
+UPDATE bookshelf_volume
+SET ratings = 5
+WHERE bookshelf_id = 5 AND volume_id = 'wrOQLV6xB-wC';
+
+
+UPDATE bookshelf_volume
+SET ratings = 5
+WHERE bookshelf_id = 5 AND volume_id = 'KUMIEAAAQBAJ';
+
+SELECT * FROM VOLUME;
